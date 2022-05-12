7e050ec32e17a8481dad04290fec200385992b93
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 19:03:51 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-timer-label',
-    version='0.2.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt QLabel adapted to timer feature',
     url='https://github.com/yjg30737/pyqt-timer-label.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-responsive-label>=0.0.1'
